cfe96ac5eb1437f70ad8dc310ef4e8e3ea163255
ogtayaliyev <ogtayaliyev9@gmail.com>
2024-06-15 22:37:44 +0200

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   // Mode de génération statique
-  target: "static",
 
-  // Configuration pour le chemin du router
-  router: {
-    base: '/portfolio/',  // Adapter selon le chemin de déploiement sur GitHub Pages ou autre
-  },
 
   // Modules de Nuxt.js
   modules: [
@@ -19,18 +14,6 @@ export default defineNuxtConfig({
     "nuxt-icon"
   ],
 
-  // Configuration de l'en-tête
-  head: {
-    title: "Ogtay Aliyev Portfolio",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Description de votre portfolio" },  // Ajouter une description pertinente
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },  // Adapter le chemin de l'icône
-    ],
-  },
 
   // Ajout de fichiers CSS globaux
   css: [
@@ -46,13 +29,7 @@ export default defineNuxtConfig({
   components: true,
 
   // Modules de build à utiliser
-  buildModules: [
-    // Module pour le support de TypeScript
-    "@nuxt/typescript-build",
-    
-    // Module pour la génération d'une version optimisée de Tailwind CSS (requis pour @nuxtjs/tailwindcss)
-    "@nuxtjs/tailwindcss"
-  ],
+
 
   // Configuration de la build
 
